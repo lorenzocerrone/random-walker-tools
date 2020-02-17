c8a4ba2e3e60e5cbb33483e66d1f867327b4bae9
lcerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2020-02-17 15:21:06 +0100

    code refactor

--- randomwalkertools/__init__.py
+++ randomwalkertools/__init__.py
@@ -1,5 +1,6 @@
 from randomwalkertools.graphtools.solvers import direct_solver, solve_cg_mg, solve_cg, solve_gpu
-from .randomwalker_algorithm import random_walker_algorithm_2d, random_walker_algorithm_3d
+from randomwalkertools.randomwalker_algorithm import random_walker_algorithm_2d, random_walker_algorithm_3d
+from randomwalkertools.differentiable_random_walker_algorithm import DifferentiableRandomWalker2D
 
 solvers = {"direct": direct_solver,
           "cg_mg": solve_cg_mg,

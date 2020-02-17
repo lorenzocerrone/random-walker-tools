8fa8b3ce55a180b7819842c73483c7e711de9429
lcerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2020-02-17 15:21:39 +0100

    code refactor

--- benchmark/benchmark2d.py
+++ benchmark/benchmark2d.py
@@ -1,6 +1,8 @@
-import numpy as np
-from randomwalkertools.randomwalker_algorithm import random_walker_algorithm_2d
 import time
+
+import numpy as np
+
+from randomwalkertools.randomwalker_algorithm import random_walker_algorithm_2d
 
 all_sizes, beta = 2 ** np.arange(4, 13), 1
 print(all_sizes)

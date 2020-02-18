eea384a6718d598d0f5f7f7f8453c56344507f8b
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2020-02-18 11:38:49 +0100

    change example

--- examples/example2D.py
+++ examples/example2D.py
@@ -3,7 +3,7 @@ import numpy as np
 from rwtools.randomwalker_algorithm import random_walker_algorithm_2d
 
 x = np.ones((128, 128))
-x[64, :] = 0
+x[64, :] = 1
 
 seeds = np.zeros_like(x).astype(np.int)
 
@@ -11,8 +11,8 @@ seeds[0, 0] = 1
 seeds[-1, -1] = 2
 
 _x = random_walker_algorithm_2d(x, seeds_mask=seeds, beta=1, offsets=((1, 0), (0, 1)), solving_mode="direct",
-                                return_prob=True, divide_by_std=False)
-print(_x.sum(-1))
+                                return_prob=False, divide_by_std=False)
+print(_x)
 
 
 
